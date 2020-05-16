cf55b629878e92e9ccb394f4ef42999877158997
jj1165922611 <1165922611@qq.com>
2020-05-16 20:46:48 +0800

    update

---
+++ python_base/base9/base9_4/test_base9_4_2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time       : 2020-05-16
+# @Author     : Joey Jiang
+# @File       : test_base9_4_2.py
+# @Software   : Pycharm
+# @Description: 参数化用例
+
+import pytest
+import yaml
+@pytest.mark.parametrize("a,b",yaml.safe_load(open("data.yml","rb")))
+def test_string(a,b):
+    print(a+b)
+@pytest.mark.parametrize(["a","b"],[(10,20),(20,30)])
+def test_list(a,b):
+    print(a+b)
+@pytest.mark.parametrize(("a","b"),[(10,20),(20,30)])
+def test_tuple(a,b):
+    print(a+b)
+if __name__ == '__main__':
+    pytest.main(['-v','-s','test_base9_4_2.py'])
---
+++ python_base/base9/base9_5/test_base9_5_1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time       : 2020-05-16
+# @Author     : Joey Jiang
+# @File       : test_base9_5_1.py
+# @SOftware   : Pycharm
+# @Description: 数据驱动
+
+import pytest
+import yaml
+class TestDemo:
+    @pytest.mark.parametrize("env",yaml.safe_load(open("./env.yml","rb")))
+    def test_demo(self,env):
+        print(env)
+        if "test" in env:
+            print("这是测试环境")
+            print((f"测试环境的ip是{env['test']}"))
+        elif "dev" in env:
+            print("这是开发环境")
