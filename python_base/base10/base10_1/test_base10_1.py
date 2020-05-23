69700f29d774001bdd9f114c0f4784b63063cede
jj1165922611 <1165922611@qq.com>
2020-05-23 13:12:26 +0800

    update

---
+++ python_base/base10/base10_1/test_base10_1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time       : 2020-05-23
+# @Author     : Joey Jiang
+# @File       : test_base10_1.py
+# @SOftware   : PyCharm
+# @Description: selenium安装
+
+from selenium import webdriver
+def test_baidu():
+    driver=webdriver.Chrome()
+    driver.get("https://www.baidu.com")
+    driver.quit()
---
+++ python_base/base10/base10_3/test_base10_3_2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time       : 2020-05-23
+# @Author     : Joey Jiang
+# @File       : test_base10_3_2.py
+# @Software   : PyCharm
+# @Description: selenium用例编写
+import time
+
+from selenium import webdriver
+
+class TestTesterHome:
+    def setup(self):
+        self.driver=webdriver.Chrome()
+        self.driver.implicitly_wait(5)
+    def teardown(self):
+        self.driver.quit()
+    def test_case(self):
+        self.driver.get("https://testerhome.com/")
+        self.driver.find_element_by_link_text("社团").click()
+        time.sleep(1)
+        self.driver.find_element_by_link_text("霍格沃兹测试学院").click()
+        time.sleep(1)
+        self.driver.find_element_by_xpath("//div[@id='main']/div/div/div/div/div/div[2]/div/a").click()
+        time.sleep(1)
