3c9de197479e1938c2305ed4b4627a411305bf31
jj1165922611 <1165922611@qq.com>
2020-05-25 22:50:16 +0800

    update

--- python_base/base11/base11_3/test_base11_3_2.py
+++ python_base/base11/base11_3/test_base11_3_2.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # @Time       : 2020-05-25
 # @Author     : Joey Jiang
-# @File       : test_base11_3_1.py
+# @File       : test_base11_3_2.py
 # @Software   : PyCharm
 # @Description: 文件上传弹框处理
 from time import sleep
@@ -21,7 +21,6 @@ class TestFileUpload:
     def test_file_upload(self):
         self.driver.get("https://image.baidu.com")
         sleep(2)
-        self.driver.find_element(By.XPATH,'//*[@id="sttb"]/img[1]').clik()
+        self.driver.find_element(By.XPATH,'//*[@id="sttb"]/img[1]').click()
         self.driver.find_element(By.ID,"stfile").send_keys("G:/python/1.png")
         sleep(3)
-        # 有问题
---
+++ python_base/base11/base11_3/test_base11_3_3.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time       : 2020-05-25
+# @Author     : Joey Jiang
+# @File       : test_base11_3_3.py
+# @Software   : PyCharm
+# @Description: 文件上传弹框处理
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+
+
+class TestAlert:
+    def setup(self):
+        self.driver=webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(3)
+    def teardown(self):
+        self.driver.quit()
+    def test_alert(self):
+        self.driver.get("https://www.runoob.com/try/try.php?filename=jqueryui-api-droppable")
+        sleep(10) # 点击页面中的显示异常
+        self.driver.switch_to.frame("iframeResult")
+        ele1=self.driver.find_element(By.ID,"draggable")
+        ele2=self.driver.find_element(By.ID,"droppable")
+        action=ActionChains(self.driver)
+        action.drag_and_drop(ele1,ele2)
+        action.perform()
+        sleep(2)
+        self.driver.switch_to.alert.accept()
+        self.driver.switch_to.parent_frame()
+        print(self.driver.find_element(By.ID, "submitBTN").text)
