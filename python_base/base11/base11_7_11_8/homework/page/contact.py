7ab3cdfc9eba802fd271bf3ec76fdc38e2001270
jj1165922611 <1165922611@qq.com>
2020-05-28 21:16:27 +0800

    update

---
+++ python_base/base11/base11_7_11_8/homework/page/contact.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: -*-
+# @Time       : 2020-05-27
+# @Author     : Joey Jiang
+# @File       : contact.py
+# @Software   : PyCharm
+# @Description: 企业微信web端自动化测试实战（一）
+from python_base.base11.base11_7_11_8.homework.page.base import BasePage
+
+
+class ContactPage(BasePage):
+    def search_member(self):
+        pass
+    def add_department(self):
+        pass
---
+++ python_base/base12/base12_1/test_base_12_1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8
+# @Time       : 2020-05-28
+# @Author     : Joey Jiang
+# @File       : test_base_12_1.py
+# @Software   : PyCharm
+# @Description: appium环境安装与架构介绍
+from appium import webdriver
+class TestAppium:
+    def test_appium(self):
+        desired_caps={}
+        desired_caps['platformName']='Android'
+        desired_caps['platformVersion']='6.0'
+        desired_caps['deviceName']='127.0.0.1:7555'
+        desired_caps['appPackage']='com.android.settings'
+        desired_caps['appActivity']='com.android.settings.Settings'
+        self.driver=webdriver.Remote('http://localhost:4723/wd/hub',desired_caps)
+        self.driver.quit()
