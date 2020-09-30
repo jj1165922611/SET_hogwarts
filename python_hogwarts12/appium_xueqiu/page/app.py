f3211c6d2e445bb7e45bc7acca981074848ebc13
jj1165922611 <1165922611@qq.com>
2020-10-01 00:40:23 +0800

    update

---
+++ python_hogwarts12/appium_xueqiu/page/app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time       : 2020-09-2
+# @Author     : Joey Jiang
+# @File       : app.py
+# @Software   : PyCharm
+# @Description: 打造自己的测试框架
+from appium.webdriver import webdriver
+
+from python_hogwarts12.appium_xueqiu.page.basepage import BasePage
+
+
+class App(BasePage):
+    _appPackage=""
+    _appActivity=""
+    def start(self):
+        if self._driver is None:
+            desired_caps={}
+            desired_caps['platformName']="Android"
+            desired_caps['platformVersion']="6.0"
+            desired_caps['appPackage']=self._appPackage
+            desired_caps['appActivity']=self._appActivity
+            desired_caps['noReset']='true'
+            self.driver=webdriver.Remote('http://127.0.0.1:4723/wd/hub',desired_caps)
+        else:
+            self._driver.launch_app()
+        self._driver.implicitly_wait(5)
+    def stop(self):
+        pass
+    def restart(self):
+        pass
+    def main(self):
+        pass
---
+++ python_hogwarts12/appium_xueqiu/page/basepage.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time       : 2020-09-29
+# @Author     : Joey Jiang
+# @File       : basepage.py
+# @Software   : PyCharm
+# @Description: 打造自己的测试框架
+from appium.webdriver.common.mobileby import MobileBy
+from appium.webdriver.webdriver import WebDriver
+
+
+class BasePage:
+    _black_list=[
+        (MobileBy.XPATH,'//*[@text="确定"]'),
+        (MobileBy.XPATH,'//*[@text="下次再说"]')
+    ]
+    _num=0
+    _max_num=5
+    def __init__(self,driver: WebDriver=None):
+        self._driver=driver
+
+    def find(self,locator,value):
+        try:
+            if isinstance(locator,tuple):
+                ele=self._driver.find_element(*locator)
+            else:
+                ele=self._driver.find_element(locator,value)
+            self._num=0
+            return ele
+        except Exception as e:
+            if self._num >self._max_num:
+                raise e
+            self._num+=1
+            for black in self._black_list:
+                ele=self._driver.find_elements(*black)
+                if len(ele)>0:
+                    ele[0].click()
+                return self.find(locator.value)
+            raise e
